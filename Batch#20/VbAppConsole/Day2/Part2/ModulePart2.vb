00de1ca520f57a5b10c2d1024961c8d29ac1d4c7
rinimarlina <rinimarlina595@gmail.com>
2022-12-16 13:54:32 +0700

    first commit

---
+++ Batch#20/VbAppConsole/Day2/Part2/ModulePart2.vb
@@ -0,0 +1,24 @@
+﻿Namespace Part2
+    Module ModulePart2
+        Sub initEmployee()
+            'create object employee
+            Dim emp1 As New Employee2(1, "Siapa sih", "Kamu", DateTime.Now, 5_000_000)
+            'display Employee Info
+            Console.WriteLine(emp1.ToString())
+
+            'cara update
+            emp1.FirstName = "Yoshhh"
+            emp1.BasicSalary = 4_000_000
+            Console.WriteLine(emp1.ToString)
+
+            Dim emp2 As New Employee2("Yuli", "Yulianto", DateTime.Now, 75000000)
+            Console.WriteLine(emp2.ToString)
+
+            Dim emp3 As New Programmer("Vidi", "Aldiana", DateTime.Now, 5600000, 100000)
+            Console.WriteLine(emp3.ToString)
+
+            Dim TotalBasicSalary As Integer
+
+        End Sub
+    End Module
+End Namespace
---
+++ Batch#20/VbAppConsole/Day2/Part2/Programmer.vb
@@ -0,0 +1,26 @@
+﻿Namespace Part2
+    Public Class Programmer
+        Inherits Employee2      'inherits, untuk deklarasi parent nya si Class
+
+        Private _transportasi As Double
+
+
+
+        Public Sub New(firstName As String, lastName As String, joinDate As Date, basicSalary As Double, Optional transportasi As Double = Nothing)
+            MyBase.New(firstName, lastName, joinDate, basicSalary)
+            _transportasi = transportasi
+        End Sub
+        Public Overrides Function ToString() As String
+            Return $" {MyBase.ToString()}
+Bonus transportasi : {_transportasi}"
+        End Function
+        Public Property Transportasi1 As Double
+            Get
+                Return _transportasi
+            End Get
+            Set(value As Double)
+                _transportasi = value
+            End Set
+        End Property
+    End Class
+End Namespace
